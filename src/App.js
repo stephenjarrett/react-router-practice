b3b8d7c1edb667658820df718d75d7e39245fd6d
Stephen <smjarr89@gmail.com>
2018-07-26 11:13:22 -0400

    added routes and Link component for  different routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Link} from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        
+	render() {
+		return (
+		<div className="App">
+			<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h1 className="App-title">Welcome to React</h1>
+			</header>
+			
+			<ul>
+				<li> <Link to="/home">Home</Link> </li>
+				<li> <Link to="/about">About</Link> </li>
+				<li> <Link to="/njoy">Njoy</Link> </li>
+			</ul>
+			
+			<Route path="/home" component={Home} /> 
+			<Route path="/about" component={About} /> 
+			<Route path="/njoy" component={Njoy} /> 
 
-        
-      </div>
-    );
-  }
+		</div>
+		);
+	}
+}
+
+const Home = (props) => {
+	return (
+		<div>
+			<h1>Welcome home</h1>
+		</div>
+	);
+}
+
+const About = (props) => {
+	return (
+		<div>
+			<h1>About this app...</h1>
+		</div>
+	);
+}
+
+const Njoy = (props) => {
+	return (
+		<div>
+			<h1>Let's Njoy</h1>
+		</div>
+	);
 }
 
 export default App;
